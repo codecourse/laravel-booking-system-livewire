4b0c81206741b1a56eddb85a28238c11e69d0a3a
Alex Garrett-Smith <alex@codecourse.com>
2024-04-25 11:26:31 +0100

    home.blade.php and service.blade.php resources

---
+++ course_resources/home.blade.php
@@ -0,0 +1,19 @@
+<div class="space-y-10">
+    <div>
+        <h2 class="text-xl font-medium">Choose a professional</h2>
+        <div class="grid grid-col-2 md:grid-cols-5 gap-8 mt-6">
+            <a href="" class="py-8 px-4 border border-slate-200 rounded-lg shadow-sm flex flex-col items-center justify-center text-center hover:bg-gray-50/75">
+                <img src="" class="rounded-full size-14">
+                <div class="text-sm font-medium mt-3 text-slate-600">
+                    [name]
+                </div>
+            </a>
+        </div>
+    </div>
+    <div>
+        <h2 class="text-xl font-medium">Or, choose a service first</h2>
+        <div class="grid grid-col-2 md:grid-cols-5 gap-8 mt-6">
+            [services]
+        </div>
+    </div>
+</div>
