54fd33ce492343249765e744d9afb7b51acb20f9
Alex Garrett-Smith <alex@codecourse.com>
2024-04-26 12:36:58 +0100

    checkout.blade.php and appointment-show.blade.php resources

---
+++ course_resources/appointment-show.blade.php
@@ -0,0 +1,32 @@
+<div class="space-y-12">
+    <div>
+        <h2 class="text-xl font-medium mt-3">Thanks, you're booked in/Cancelled</h2>
+        <div class="flex mt-6 space-x-3 bg-slate-100 rounded-lg p-4">
+            <img src="" class="rounded-lg size-14 bg-slate-100">
+            <div class="w-full">
+                <div class="flex justify-between">
+                    <div class="font-semibold">
+                        [service title] ([duration] minutes)
+                    </div>
+                    <div class="text-sm">
+                        [price]
+                    </div>
+                </div>
+                <div class="text-sm">
+                    [employee name]
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div>
+        <h2 class="text-xl font-medium mt-3">When</h2>
+        <div class="mt-6 bg-slate-100 rounded-lg p-4">
+            [time]
+        </div>
+    </div>
+
+    <form>
+        <button class="text-blue-500">Cancel appointment</button>
+    </form>
+</div>
---
+++ course_resources/checkout.blade.php
@@ -0,0 +1,57 @@
+<form class="space-y-10">
+    <div>
+        <h2 class="text-xl font-medium">Here's what you're booking</h2>
+        <div class="mt-6 flex space-x-3 bg-slate-100 rounded-lg p-4">
+            <img src="" class="rounded-lg size-14 shrink-0">
+            <div class="rounded-lg size-14 bg-slate-200 shrink-0"></div>
+
+            <div class="w-full flex justify-between">
+                <div>
+                    <div class="font-semibold">Service title (duration minutes)</div>
+                    <div>Employee name/Any employee</div>
+                </div>
+                <div>
+                    $price
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div>
+        <h2 class="text-xl font-medium">1. When for?</h2>
+        <input class="mt-6 text-sm bg-slate-100 border-0 rounded-lg px-6 py-4 w-full" placeholder="Choose a date">
+    </div>
+
+    <div>
+        <h2 class="text-xl font-medium">2. Choose a slot</h2>
+        <div class="mt-6">
+            <div class="grid grid-cols-3 md:grid-cols-5 gap-8">
+                <button type="button" class="py-3 px-4 text-sm border border-slate-200 rounded-lg text-center hover:bg-gray-50/75 cursor-pointer">
+                    09:00
+                </button>
+            </div>
+        </div>
+    </div>
+
+    <div>
+        <h2 class="text-xl font-medium">2. Your details and book</h2>
+
+        <div class="bg-slate-900 text-white py-4 px-6 rounded-lg mt-3">
+            Error
+        </div>
+
+        <div class="mt-6">
+            <div>
+                <label for="name" class="sr-only">Your name</label>
+                <input type="text" name="name" id="name" class="mt-1 text-sm bg-slate-100 border-0 rounded-lg px-6 py-4 w-full" placeholder="Your name">
+            </div>
+            <div class="mt-3">
+                <label for="email" class="sr-only">Your email</label>
+                <input type="text" name="email" id="email" class="mt-1 text-sm bg-slate-100 border-0 rounded-lg px-6 py-4 w-full" placeholder="Your email">
+            </div>
+            <button type="submit" class="mt-6 py-3 px-6 text-sm border border-slate-200 rounded-lg flex flex-col items-center justify-center text-center hover:bg-slate-900 cursor-pointer bg-slate-800 text-white font-medium">
+                Make booking
+            </button>
+        </div>
+    </div>
+</form>
