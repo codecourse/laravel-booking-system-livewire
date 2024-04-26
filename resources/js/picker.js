873f09ff91712e99b655d1c3f9cc1e1089eaf24e
Alex Garrett-Smith <alex@codecourse.com>
2024-04-26 10:33:45 +0100

    Episode 25

--- resources/js/picker.js
+++ resources/js/picker.js
@@ -1,5 +1,15 @@
+import { easepick} from "@easepick/bundle";
+import style from '@easepick/bundle/dist/index.css?url'
+
 export default function (Alpine) {
-    Alpine.directive('picker', () => {
-        console.log('picker registered')
+    Alpine.directive('picker', (el) => {
+        const picker = new easepick.create({
+            element: el,
+            readonly: true,
+            zIndex: 50,
+            css: [
+                style
+            ]
+        })
     })
 }
