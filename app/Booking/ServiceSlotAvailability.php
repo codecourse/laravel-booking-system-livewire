7781a35084020d802508edd08e993cf1b4a03eae
Alex Garrett-Smith <alex@codecourse.com>
2024-04-26 09:27:30 +0100

    Episode 18

--- app/Booking/ServiceSlotAvailability.php
+++ app/Booking/ServiceSlotAvailability.php
@@ -6,6 +6,7 @@ use App\Models\Employee;
 use App\Models\Service;
 use Carbon\Carbon;
 use Illuminate\Support\Collection;
+use Spatie\Period\Period;
 
 class ServiceSlotAvailability
 {
@@ -15,11 +16,25 @@ class ServiceSlotAvailability
     {
         $range = (new SlotGenerator($startsAt, $endsAt))->generate($this->service->duration);
 
-        $this->employees->each(function (Employee $employee) {
-            // get availability for employee
-            // remove appointments for employee
-            // add available employees to slots
-            // clear up empty slots
+        $this->employees->each(function (Employee $employee) use ($startsAt, $endsAt, &$range) {
+            $periods = (new ScheduleAvailability($employee, $this->service))->forPeriod($startsAt, $endsAt);
+
+            foreach ($periods as $period) {
+                $this->addAvailableEmployeeForPeriod($range, $period, $employee);
+            }
+        });
+
+        dd($range);
+    }
+
+    protected function addAvailableEmployeeForPeriod(DateCollection $range, Period $period, Employee $employee)
+    {
+        $range->each(function (Date $date) use ($period, $employee) {
+            $date->slots->each(function (Slot $slot) use ($period, $employee) {
+                if ($period->contains($slot->time)) {
+                    $slot->addEmployee($employee);
+                }
+            });
         });
     }
 }
--- app/Booking/Slot.php
+++ app/Booking/Slot.php
@@ -2,11 +2,21 @@
 
 namespace App\Booking;
 
+use App\Models\Employee;
 use Carbon\Carbon;
+use Illuminate\Support\Collection;
 
 class Slot
 {
+    public Collection $employees;
+
     public function __construct(public Carbon $time)
     {
+        $this->employees = collect();
+    }
+
+    public function addEmployee(Employee $employee)
+    {
+        $this->employees->push($employee);
     }
 }
